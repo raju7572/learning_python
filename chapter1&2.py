1c92a13092049b7b71c1743767fb9fd10b86f788
raju7572 <chraj818@gmail.com>
2019-10-16 20:14:39 +0530

    Practice Chapter 1 and 2

---
+++ chapter1&2.py
@@ -0,0 +1,165 @@
+
+
+'hello,world!'
+
+type('hello,world')
+
+type(17)
+
+type('3.5')
+
+1,000,000
+
+message="and now for something completely different"
+
+n=17
+
+pi=3.1415926535897932
+
+type(message)
+
+type(n)
+
+type(pi)
+
+zipcode=02132
+
+zipcode
+
+1114
+
+# topic1
+
+76trombones='big parade'
+
+more@=10000
+
+class='advanced therotical zymurgy'
+
+minute=59
+
+minute/60
+
+minute/60.0
+
+miles=26.2
+
+miles*1.61
+
+miles=26.2
+
+print miles*1.61
+
+5
+
+x=5
+
+x+1
+
+a=5.5
+
+b=2.2
+
+c=a//b
+
+
+print(c)
+
+percentage=(minute*100)/60  #percentage of an hour
+
+print(percentage)
+
+v=5  #velocity in meters/second
+
+print(v)
+
+width=17
+
+height=12.0
+
+delimeter='.'
+
+width/2
+
+width/2.0
+
+height/3
+
+1+2*5
+
+delimeter*5
+
+pi
+
+4/3 * pi * 5 ** 3
+
+5 ** 2
+
+chapter3
+types conversion
+
+int(32)    #int to integer
+
+int("hello")
+
+int(3.9999)
+
+int(-2.3)
+
+float(32)
+
+float('3.14519')
+
+str(32)
+
+str(3.14159)
+
+import math as math
+
+math.sin(degrees/360*2*math.pi)
+
+import math as math
+
+degrees=45
+
+radians=degrees/360.0*2*math.pi
+
+math.sin(radians)
+
+def print_lyrics():
+    print("iam a lumber jack,and iam okay")
+    print("i sleep all night and i work all day")
+    def repeat_lyrics():
+        print_lyrics()
+        print_lyrics
+        repeat_lyrics
+
+repeat_lyrics():
+    def print_lyrics():
+        print("iam a lumber jack,and iam okay")
+        print("i sleep all night and i work all the day")
+        def repeat_lyrics():
+            print_lyrics
+            print_lyrics()
+
+print("iam a lumber jack,and iam okay")
+print("i sleep all nifht and i work all the day")
+def repeat_lyrics():
+    callback("i sleep all the night and i work all the day")
+    print_lyrics
+
+
+def print_twice(bruce)
+print(bruce)
+
+line1='bing tiddle'
+line2='tiddle bang'\
+cat_twice(line1,line2)
+print(cat)
+
+from math import*
+cos(pi)
+
+from math import*
+print(pi)
+
